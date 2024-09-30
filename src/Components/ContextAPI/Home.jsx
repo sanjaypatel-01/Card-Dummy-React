0db9e9d8449a4f3355f4e5735967ab94448c8fb6
Sanjay Patel <sanjaypatel5346@gmail.com>
2024-09-30 11:41:24 +0530

    added some changes

--- src/Components/ContextAPI/Home.jsx
+++ src/Components/ContextAPI/Home.jsx
@@ -11,3 +11,55 @@ function Home() {
 }
 
 export default Home;
+
+
+// App.jsx
+
+// import React, { useState } from "react";
+// import axios from "axios";
+// import Home from "./Components/ContextAPI/Home";
+// import { Link, Route, Routes } from "react-router-dom";
+// import User from "./Components/ContextAPI/User";
+// import About from "./Components/ContextAPI/About";
+// import UserDetails from "./Components/ContextAPI/UserDetails";
+
+// function App() {
+//   return (
+//     <div className=" w-full h-screen p-6">
+//       <nav className="flex justify-center gap-6 mb-12">
+//         <Link to="/">Home</Link>
+//         <Link to="/user">User</Link>
+//         <Link to="/about">About</Link>
+//       </nav>
+
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/user" element={<User />} />
+//         <Route path="/user/:id" element={<UserDetails />} />
+//         <Route path="/about" element={<About />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// main.jsx
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import { BrowserRouter } from 'react-router-dom'
+// import Context from './Components/ContextAPI/Context.jsx'
+
+// createRoot(document.getElementById('root')).render(
+  
+//   <Context>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Context>
+  
+// )
